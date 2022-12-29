dd12de4b9af9e10226d32ff8a07964b2d03848ae
Walker Le <walker.le88@gmail.com>
2022-12-28 21:33:55 -0700

    commit tree 5ad84b2148fe33f5cb17c996f1313659c2a8a08b

--- index.js
+++ index.js
@@ -12,5 +12,32 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(stringElement => {
+    return stringManip(stringElement); // datatype: string
+  })
 }
+
+function stringManip(string) {
+  const arrayOfEachWord = string.split(" "); // datatype: string to array
+
+  return arrayOfWords(arrayOfEachWord).join(' '); // datatype: array to string AFTER invoking arrayOfWords()
+}
+
+function arrayOfWords(array) {
+  // const newArray = []; // .map potential
+  // for(const element of array) {
+  //   const newElement = element.toUpperCase().substring(0, 1) + element.slice(1);
+  //   newArray.push(newElement);
+  // }
+  // return newArray;
+
+  // .map attempt
+  return array.map(element => {
+    return element.toUpperCase().substring(0, 1) + element.slice(1) // returns a string element and places into copied array
+  }) // returns an array
+}
+
+// stringManip(tutorials[0]);
+
+// titleCased();
+console.log(titleCased());
